cd07189c14e964a25efe40e4393fe9e47711df49
Sigosu <szymon.sciegienka@gmail.com>
2022-02-11 22:11:53 +0100

    Structural changes fixing imports.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Lingus",
-    version="0.0.1",
+    version="0.0.2",
     author="Sigosu",
     author_email="s.sigosu@gmail.com",
     description="Unofficial Duolingo API package",
